8b812e0b31d12e42c192654aca6a77ea00c44605
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-08-30 16:11:53 -0400

    made Users struct

--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Models/RandomUserStruct.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Models/RandomUserStruct.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+struct Users: Codable {
+    let results: [InfoWrapper]
+    
+    static func getUsers(from data: Data) -> [InfoWrapper] {
+        do {
+            let users = try JSONDecoder().decode(Users.self, from: data)
+            return users.results
+        } catch {
+            print(error)
+        }
+        return [InfoWrapper]()
+    }
+}
+struct InfoWrapper: Codable {
+    let name: Name
+    let location: Location
+    let email: String
+    let phone: String
+    let nat: String
+    let picture: Picture
+}
+struct Name: Codable {
+    let title: String
+    let first: String
+    let last: String
+}
+
+struct Location: Codable {
+    let street: String
+    let city: String
+    let state: String
+}
+
+struct Picture: Codable {
+    let medium: String
+}
