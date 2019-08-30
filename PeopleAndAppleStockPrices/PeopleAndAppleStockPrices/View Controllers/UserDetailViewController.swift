a7793eaef3cf7ea8672923cdbdad33974b2fbcd6
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-08-30 17:47:39 -0400

    adds userDetailVC and outlets for labels and image

---
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/View Controllers/UserDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  UserDetailViewController.swift
+//  PeopleAndAppleStockPrices
+//
+//  Created by Angela Garrovillas on 8/30/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var nationalityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
